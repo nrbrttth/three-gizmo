2f6c825145ee02b977db748c355933d44a612034
Norbert Tóth <norbert.toth.94@gmail.com>
2024-10-23 00:12:11 +0200

    Change default colors and size

--- components/move.js
+++ components/move.js
@@ -1,88 +1,83 @@
-import * as THREE from 'three';
-import { parse } from "../lib/object";
-import { mouseRay } from "../lib/raycaster";
+import * as THREE from 'three'
+import { parse } from "../lib/object"
+import { mouseRay } from "../lib/raycaster"
 export { move }
-import { setMaterial } from '../lib/setMaterial';
-import { objectContains } from '../lib/objectContains';
-import { getColor } from '../lib/colors';
+import { setMaterial } from '../lib/setMaterial'
+import { objectContains } from '../lib/objectContains'
+import { getColor } from '../lib/colors'
 
-import { Object } from '../lib/objects';
+import { Object } from '../lib/objects'
 
-function move(canvas, camera, renderer, position, callback, endCallback)
-{
+function move(canvas, camera, renderer, position, callback, endCallback) {
     let active = false
-    const pivotScene = new THREE.Scene();
-    const material1 = new THREE.MeshBasicMaterial({ color: getColor("color1")});
-    const material2 = new THREE.MeshBasicMaterial({ color: getColor("color2")});
-    const material3 = new THREE.MeshBasicMaterial({ color: getColor("color3") });
+    const pivotScene = new THREE.Scene()
+    const material1 = new THREE.MeshBasicMaterial({ color: getColor("color1") })
+    const material2 = new THREE.MeshBasicMaterial({ color: getColor("color2") })
+    const material3 = new THREE.MeshBasicMaterial({ color: getColor("color3") })
 
-    const material4 = new THREE.MeshBasicMaterial({ color: getColor("hover")});
-    
+    const material4 = new THREE.MeshBasicMaterial({ color: getColor("hover") })
+
     const x = parse(Object("move_helper"))
 
     const y = x.clone()
     const z = x.clone()
 
     // x.children[0].geometry.translate(0,0,-1)
-    const xz =parse(Object("generic_two_axis_helper"))
+    const xz = parse(Object("generic_two_axis_helper"))
 
 
 
-    xz.children[0].geometry.translate(2,0,-1.5)
-    xz.children[0].geometry.scale(3,3,3)
-    
+    xz.children[0].geometry.translate(2, 0, -1.5)
+    xz.children[0].geometry.scale(3, 3, 3)
+
     const xy = xz.clone()
     const yz = xz.clone()
 
 
-    xy.rotation.x = -Math.PI / 2;
-    yz.rotation.z = Math.PI / 2;
+    xy.rotation.x = -Math.PI / 2
+    yz.rotation.z = Math.PI / 2
 
     pivotScene.add(xz)
     pivotScene.add(xy)
     pivotScene.add(yz)
     pivotScene.add(y)
     pivotScene.add(z)
-    pivotScene.add(x); 
+    pivotScene.add(x)
 
-    y.rotation.x = Math.PI / 2;
-    z.rotation.y = Math.PI;
+    y.rotation.x = Math.PI / 2
+    z.rotation.y = Math.PI
     x.rotation.y = -Math.PI / 2
 
-    function resetMaterial()
-    {
-        setMaterial(x,material1)
-        setMaterial(y,material2)
-        setMaterial(z,material3)
-        setMaterial(xz,material1)
-        setMaterial(xy,material2)
-        setMaterial(yz,material3)
+    function resetMaterial() {
+        setMaterial(x, material1)
+        setMaterial(y, material2)
+        setMaterial(z, material3)
+        setMaterial(xz, material1)
+        setMaterial(xy, material2)
+        setMaterial(yz, material3)
         canvas.style.cursor = "default"
 
     }
     resetMaterial()
 
-    
+
     let drag = false
     let selectedAxis = null
     let selected = null
     let previousDiff = null
     let isHover = false
 
-    function move(point, axis)
-    {
-        position[axis] = point[axis];
+    function move(point, axis) {
+        position[axis] = point[axis]
     }
-    function scaleObject(object)
-    {
+    function scaleObject(object) {
         // Calculate the distance between the camera and the object
-        const distance = camera.position.distanceTo(position) / 80;
+        const distance = camera.position.distanceTo(position) / 160
 
         // Set the scale of the object based on distance
-        object.scale.set(distance, distance, distance);
+        object.scale.set(distance, distance, distance)
     }
-    function scale()
-    {
+    function scale() {
         x.position.copy(position)
         y.position.copy(position)
         z.position.copy(position)
@@ -98,11 +93,10 @@ function move(canvas, camera, renderer, position, callback, endCallback)
     }
 
 
-    document.addEventListener("mouseup",e=>{
-        if(!active) return
-        if(e.button != 0) return
-        if(drag)
-        {
+    document.addEventListener("mouseup", e => {
+        if (!active) return
+        if (e.button != 0) return
+        if (drag) {
             endCallback(position)
         }
         drag = false
@@ -111,92 +105,79 @@ function move(canvas, camera, renderer, position, callback, endCallback)
 
     const caster = mouseRay(canvas, camera)
 
-    document.addEventListener("mousedown",e=>{
-        if(!active) return
-        if(e.button != 0 || e.controlKey) return
-        if(selectedAxis)
-        {
+    document.addEventListener("mousedown", e => {
+        if (!active) return
+        if (e.button != 0 || e.controlKey) return
+        if (selectedAxis) {
             const ray = caster.cast(e)
             drag = true
             const point = ray.snap(selectedAxis, position)
             previousDiff = {}
-            for(const axis of selectedAxis)
-            {
+            for (const axis of selectedAxis) {
                 previousDiff[axis] = position[axis] - point[axis]
             }
         }
-    },true)
+    }, true)
 
-    document.addEventListener("mousemove", e=>{
-        if(!active) return
-        if(e.button != 0 || e.ctrlKey) 
-        {
+    document.addEventListener("mousemove", e => {
+        if (!active) return
+        if (e.button != 0 || e.ctrlKey) {
             resetMaterial()
             return
         }
-        
-        if(drag)
-        {
+
+        if (drag) {
             canvas.style.cursor = "grabbing"
 
             const ray = caster.cast(e)
             const point = ray.snap(selectedAxis, position)
-            for(const axis of selectedAxis)
-            {
+            for (const axis of selectedAxis) {
                 point[axis] += previousDiff[axis]
                 move(point, axis)
             }
-            
+
             callback(position)
         }
-        else
-        {
-            const ray = caster.cast(e,[x,y,z,xz,xy,yz])
+        else {
+            const ray = caster.cast(e, [x, y, z, xz, xy, yz])
             resetMaterial()
             selectedAxis = null
             isHover = false
-            if(ray.intersect != null)
-            {
+            if (ray.intersect != null) {
                 isHover = true
-                if(objectContains(x, ray.intersect.object))
-                {
+                if (objectContains(x, ray.intersect.object)) {
                     selected = x
                     selectedAxis = "x"
                     setMaterial(ray.intersect.object, material4)
                     canvas.style.cursor = "grab"
 
-                } 
-                if(objectContains(y, ray.intersect.object)) 
-                {
+                }
+                if (objectContains(y, ray.intersect.object)) {
                     selected = y
                     selectedAxis = "y"
                     setMaterial(ray.intersect.object, material4)
                     canvas.style.cursor = "grab"
 
                 }
-                if(objectContains(z, ray.intersect.object))
-                {
+                if (objectContains(z, ray.intersect.object)) {
                     selected = z
                     selectedAxis = "z"
                     setMaterial(ray.intersect.object, material4)
                     canvas.style.cursor = "grab"
                 }
-                if(objectContains(xz, ray.intersect.object))
-                {
+                if (objectContains(xz, ray.intersect.object)) {
                     selected = xz
                     selectedAxis = "xz"
                     setMaterial(ray.intersect.object, material4)
                     canvas.style.cursor = "grab"
                 }
-                if(objectContains(xy, ray.intersect.object))
-                {
+                if (objectContains(xy, ray.intersect.object)) {
                     selected = xy
                     selectedAxis = "xy"
                     setMaterial(ray.intersect.object, material4)
                     canvas.style.cursor = "grab"
                 }
-                if(objectContains(yz, ray.intersect.object))
-                {
+                if (objectContains(yz, ray.intersect.object)) {
                     selected = yz
                     selectedAxis = "yz"
                     setMaterial(ray.intersect.object, material4)
@@ -206,20 +187,18 @@ function move(canvas, camera, renderer, position, callback, endCallback)
         }
     })
 
-    function render()
-    {
-        if(!active) return
+    function render() {
+        if (!active) return
         scale()
-        renderer.clearDepth();
+        renderer.clearDepth()
         renderer.render(pivotScene, camera)
     }
-    function set(x,y,z)
-    {
+    function set(x, y, z) {
         position.x = x
         position.y = y
         position.z = z
         callback(position)
     }
-    return { render, set active(value){ active = value}, set, get isHover(){return isHover} }
+    return { render, set active(value) { active = value }, set, get isHover() { return isHover } }
 }
 
--- gizmo.js
+++ gizmo.js
@@ -1,37 +1,35 @@
-import * as THREE from 'three';
-import { move } from "./components/move";
-import { rotate } from "./components/rotate";
-import { scale } from "./components/scale";
+import * as THREE from 'three'
+import { move } from "./components/move"
+import { rotate } from "./components/rotate"
+import { scale } from "./components/scale"
 export { gizmo }
 export { setColor } from "./lib/colors"
-function gizmo(camera, renderer)
-{
-    renderer.autoClear = false;
-    
-    let position = new THREE.Vector3(0,0,0)
+
+function gizmo(camera, renderer) {
+    renderer.autoClear = false
+
+    let position = new THREE.Vector3(0, 0, 0)
     let helpers = []
     let events = {}
-    let object = {render, on}
-    
+    let object = { render, on }
+
     object.none = none
     object.selectedTool = "none"
 
 
-    function none()
-    {
+    function none() {
         object.selectedTool = "none"
-        for(const helper of helpers)
-        {
+        for (const helper of helpers) {
             helper.active = false
         }
     }
 
-    object.setOrigin = (point) =>{
+    object.setOrigin = (point) => {
         position.copy(point)
     }
 
-    const scalePivot = scale(renderer.domElement, camera, renderer, position,x => events?.scale(x) , x=> events["end-scale"] && events["end-scale"](x) )
-    
+    const scalePivot = scale(renderer.domElement, camera, renderer, position, x => events?.scale(x), x => events["end-scale"] && events["end-scale"](x))
+
     helpers.push(scalePivot)
 
     object.scale = () => {
@@ -41,11 +39,11 @@ function gizmo(camera, renderer)
     }
 
     object.setScale = (...props) => scalePivot.set(...props)
-    
+
     const movePivot = move(renderer.domElement, camera, renderer, position, x => {
         position.copy(x)
         events?.move(x)
-    }, x=> events["end-move"] && events["end-move"](x) )
+    }, x => events["end-move"] && events["end-move"](x))
 
     helpers.push(movePivot)
 
@@ -58,9 +56,9 @@ function gizmo(camera, renderer)
     object.setPosition = (...props) => movePivot.set(...props)
 
 
-    
-    const rotatePivot =rotate(renderer.domElement, camera, renderer, position, x => events?.rotate(x), x=>  events["end-rotate"] && events["end-rotate"](x) )
-    
+
+    const rotatePivot = rotate(renderer.domElement, camera, renderer, position, x => events?.rotate(x), x => events["end-rotate"] && events["end-rotate"](x))
+
     helpers.push(rotatePivot)
     object.rotate = () => {
         none()
@@ -70,20 +68,17 @@ function gizmo(camera, renderer)
     object.setRotation = (...props) => rotatePivot.set(...props)
 
     object.isHover = () => {
-        return helpers.some(x=>x.isHover)
+        return helpers.some(x => x.isHover)
     }
 
 
-    function render()
-    {
-        for(const helper of helpers)
-        {
+    function render() {
+        for (const helper of helpers) {
             helper.render()
         }
     }
 
-    function on(key, value)
-    {
+    function on(key, value) {
         events[key] = value
     }
 
